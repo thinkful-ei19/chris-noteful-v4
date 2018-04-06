4abf74104be9e6404e407120e8a9f8fa5c09d764
Christopher <cjszk@hotmail.com>
2018-04-06 11:01:40 -0700

    setup CICD

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-ap
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
 
-exports.JWT_SECRET = process.env.JWT_SECRET || 'rosebud';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
